8704af7e393d79850ed5962f51b76d412784ad70
Hilel Hamidi <hilel.hamidi@gmail.com>
2022-11-21 16:53:37 +0100

    consigne et route ok

---
+++ pokedex/app/Http/Controllers/PokedexController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PokedexController extends Controller
+{
+    
+//7 Route
+public function index(){
+    //Retourne une liste HTML des pokémons du pokedex
+
+}
+
+public function create(){
+    //Retourne un formulaire HTML permettant de créer un pokemon 
+    //(nom, type, attaque, défense et PV)
+
+}
+
+public function store(){
+    //Vérifie que le nom, l'attaque, la défense et les PV sont bien 
+    //reçus depuis le body de la requête
+//Retourne un message HTML de confirmation et un récapitualitf du 
+//pokemon créé (même si le pokemon n'est pas réellement enregistré)
+
+}
+
+public function show($pokemon){
+    //	Retourne en HTML les détails d'un pokemon s'il existe dans 
+    //le tableau pokedex sinon retourne un message d'erreur et un code HTTP 404
+
+}
+
+public function edit($pokemon){
+
+    //	Retourne un formulaire HTML permettant de modifier les informations 
+    //d'un pokemon (pré-repmpli avec les informations existantes). 
+    //Si le pokemon n'existe pas dans le tableau, retourne un message d'erreur et un code HTTP 404
+}
+
+public function update($pokemon){
+    //Vérifie que le nom du pokemon existe bien dans le tableau pokedex sinon 
+    //retourne un message d'erreur HTML et un code HTTP 404
+    //Vérifie que le nom, l'attaque, la défense et les PV sont bien 
+    //reçus depuis le body de la requête
+    //Retourne un message HTML de confirmation et un récapitualitf du 
+    //pokemon modifié (même si le pokemon n'est pas réellement enregistré)
+
+}
+
+public function destroy($pokemon){
+
+    //Vérifie que le nom du pokemon existe bien dans le tableau pokedex 
+    //sinon retourne un message d'erreur HTML et un code HTTP 404
+    //Retourne un message HTML de confirmation et le nom du pokemon 
+    //supprimé (même si le pokemon n'est pas réellement supprimé)
+
+}
+
+
+    
+}
--- pokedex/routes/web.php
+++ pokedex/routes/web.php
@@ -1,18 +1,21 @@
 <?php
-
+use App\Http\Controllers\PokedexController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get(	'/pokemons',[PokedexController::class,'index'])->name('pokedex.index') ;
+Route::get('/pokemons/create',[PokedexController::class,'create'])->name('pokedex.create') ;
+Route::post('/pokemons',[PokedexController::class,'store'])->name('pokedex.store') ;
+Route::get('/pokemons/{pokemon}',[PokedexController::class,'show'])->name('pokedex.show') ;
+Route::get('/pokemons/{pokemon}/edit',[PokedexController::class,'edit'])->name('pokedex.edit');
+Route::put('/pokemons/{pokemon}',[PokedexController::class,'update'])->name('pokedex.update');
+Route::delete('/pokemons/{pokemon}',[PokedexController::class,'destroy'])->name('pokedex.destroy');
+
+
