cd1ca3b285f262debe22095cc9b4de52bb6848e8
Hilel Hamidi <hilel.hamidi@gmail.com>
2022-11-22 10:33:44 +0100

    ok

--- pokedex/app/Http/Controllers/PokedexController.php
+++ pokedex/app/Http/Controllers/PokedexController.php
@@ -6,22 +6,68 @@ use Illuminate\Http\Request;
 
 class PokedexController extends Controller
 {
-    public function tableau(){ $pokedex=[
+    protected $pokedex=[
         'Bulbizarre' => [ 'type' => 'Plante', 'attaque' => 49, 'defense' => 49, 'pv' => 45, ], 
         'Salamèche' => [ 'type' => 'Feu', 'attaque' => 52, 'defense' => 43, 'pv' => 39, ], 
         'Carapuce' => [ 'type' => 'Eau', 'attaque' => 48, 'defense' => 65, 'pv' => 44, ]
                                         ];
-                             }
+                             
     
     //7 Route
 public function index(){
     //Retourne une liste HTML des pokémons du pokedex
+    $html='';
+    foreach ($this->pokedex as $key => $value) {
+        $html .= '<div>';
+        $html .= '<h2>'.$key.'</h2>';
+        $html .= '<ul>';
+        $html .= '<li>'.$value['type'].'</li>';
+        $html .= '<li>'.$value['attaque'].'</li>';
+        $html .= '<li>'.$value['defense'].'</li>';
+        $html .= '<li>'.$value['pv'].'</li>';
+        $html .= '<ul>';
+        $html .= '</div>';
+    }
+    return response($html, 200)->header('Content-Type', 'text/html');
+
 
 }
 
 public function create(){
     //Retourne un formulaire HTML permettant de créer un pokemon 
     //(nom, type, attaque, défense et PV)
+    $html='';
+    $html .= '<div>';
+    $html .= '<h2>Création de Pokemon</h2>';
+    $html .= '<form action="/pokemons" method="post">';
+    $html .= '<label for="">Nom:
+                    <input type="text">
+                </label>
+                <p>
+                <label for="">Type: 
+                    <input type="text">
+                </label>
+                </p>
+                <p>
+                <label for="">Attaque: 
+                    <input type="number">
+                </label>
+                </p>
+                <p>
+                <label for="">Défense: 
+                    <input type="number">
+                </label>
+                </p>
+                <p>
+                <label for="">PV:
+                    <input type="number">
+                </label>
+                </p>
+                <input type="submit" value="soumettre">
+            </form> ';
+    $html .= '</div>';
+    
+    return response($html, 200)->header('Content-Type', 'text/html');
 
 }
 
